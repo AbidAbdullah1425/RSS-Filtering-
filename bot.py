e79f3bf7719bad3b2029b56f30a174e116d09b7e
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2024-11-26 11:56:49 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,14 +11,14 @@ from datetime import datetime
 import pyrogram.utils
 pyrogram.utils.MIN_CHANNEL_ID = -1009147483647
 
-from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, GROUP_ID, PORT
+from config import API_HASH, API_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, GROUP_ID, PORT
 
 class Bot(Client):
     def __init__(self):
         super().__init__(
             name="Bot",
             api_hash=API_HASH,
-            api_id=APP_ID,
+            api_id=API_ID,
             plugins={
                 "root": "plugins"
             },
